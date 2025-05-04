f6e540cd4e040c3e3ca23c4fb820305e6fb2c361
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-05-04 21:33:19 +0530

    Learning Dsa

--- 02_Conditional/lec3.cpp
+++ 02_Conditional/lec3.cpp
@@ -2,6 +2,71 @@
 using namespace std;
 
 int main() {
+//Take positve integer input from user and check if it is even or odd
+int n;
+cout<<"enter the numnber ";
+cin>>n;
+if(n%2==0){
+    cout<<"even number"<<endl;
+}
+else{
+cout<<"odd number"<<endl;}  
+
+//divisible by 5 or not 
+if (n%5==0){
+    cout<<endl<<"number is divisible by five";
+}
+
+//absolute value 
+if (n<0){
+    n=-n;
+}
+cout<<endl<<"absolute value of n is n";
+
+//profit loss
+int cp , sp ;
+cout<<endl<<"enter the cost price";
+cin>>cp;
+cout<<endl<<"Enter the selling price";
+cin>>sp;
+if(sp>cp){
+    cout<<endl<<"Your profit is "<<sp-cp;
+}
+else if(cp==sp){
+cout<<endl<<"your profit is 0";
+}
+else{
+    cout<<endl<<"Your loss is "<<cp-sp;
+}
+
+// n is three digit or not 
+if(n>99 && n<1000){
+    cout<<endl<<" Given number is three digit number";
+}
+if(n>99 and n<1000){
+    cout<<endl<<" Given number is three digit number";
+}
+
+//divisible by 5 or 3
+if(n%5==0 or n%3==0){
+    cout<<endl<<" Given number is divisible by 5 or 3";
+}
+if(n%5==0 || n%3==0){
+    cout<<endl<<" Given number is divisible by 5 or 3";
+}
+
+// take 3 number and print gratest of them 
+int a ,b,c;
+cout<<endl<<"Enter The 3  number";
+cin>>a>>b>>c;
+if(a>=b){
+    if (c>a) cout<<endl<<c<<" is greatest";
+    else cout<<endl<<a<<" is the greatest";
+}
+else{
+    if (c>b) cout<<endl<<c<<" is greatest";
+    else cout<<endl<<b<<" is the greatest";
+}
 
     return 0;
 }
---
+++ 02_Conditional/lec4.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weekNumber;
+
+    cout << "Enter week number (1-7): ";
+    cin >> weekNumber;
+
+    switch (weekNumber) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid week number! Please enter a number between 1 and 7." << endl;
+    }
+
+    char operation;
+    double num1, num2, result;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator! Please use +, -, *, or /." << endl;
+    }
+
+    return 0;
+}
