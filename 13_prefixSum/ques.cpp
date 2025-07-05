8f5d42ff0bba8074c40f8ac9a1d028391a7d9fee
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-07-05 10:18:29 +0530

    prefixSum

--- 13_prefixSum/ques.cpp
+++ 13_prefixSum/ques.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+    int arr[]={1,2,3,4,5,15};
+    int n =sizeof(arr)/sizeof(arr[0]);
+int pre[n];
+pre[0]=arr[0];
+for (int i =1; i<n ; i++){
+    pre[i]=arr[i]+pre[i-1];
+    cout<<pre[i]<<endl;
+     
+}
+for (int i =0;i<n-1;i++){
+    if ((2*pre[i]-pre[n-1])==0) {cout<<i;break;}
     
+}
+
     return 0;
 }
