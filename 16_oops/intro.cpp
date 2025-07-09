8579da3b99926685842619758d12283cdfe99a57
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-07-09 20:09:34 +0530

    student class

--- 16_oops/intro.cpp
+++ 16_oops/intro.cpp
@@ -25,7 +25,29 @@ public:
     }
 
 };
+
+
+class student{
+private:
+string name ;
+int standard ;
+int rollno;
+public:
+void setName(string name){this->name=name;}
+void setRollno(int rollno){this->rollno=rollno;}
+void setStandard(int standard){this->standard=standard;}
+
+void gettName(){cout<<endl<<name;}
+void gettRollno(){cout<<endl<<rollno;}
+void gettStandard(){cout<<endl<<standard;}
+
+
+
+
+
+};
 int main() {
+//Player
     Player amit ;
     amit.name="amit";
     amit.health=100;
@@ -38,5 +60,16 @@ int main() {
 
 amit.showHealth();
 amit.showScore();
+
+//Student
+student raj;
+raj.setName("raj");
+raj.setStandard(10);
+raj.setRollno(37);
+raj.gettName();
+raj.gettStandard();
+raj.gettRollno();
+
+
     return 0;
 }
