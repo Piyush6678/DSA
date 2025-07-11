e8f472ba7c2a2e221bedee511e3f79a8127f786b
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-07-11 08:45:09 +0530

    initialisation list

--- 16_oops/oops2.cpp
+++ 16_oops/oops2.cpp
@@ -3,6 +3,7 @@ using namespace std;
 //constructors 
 class Bike{
 public:
+static int noOfBikes; //this belongs to class
 int tyreSize;
 int engineSize;
 
@@ -13,7 +14,11 @@ int engineSize;
 Bike(int tyreSize,int engineSize=150){
     this->tyreSize=tyreSize;
     this->engineSize=engineSize;
+    noOfBikes++;
 }
+
+//initialisation list 
+// Bike(int ts,int es):tyreSize(ts),engineSize(es){}
 
 
 
@@ -26,20 +31,25 @@ Bike(int tyreSize,int engineSize=150){
 
 };
 //static
-void print(){
-static int b =10;
-cout<<b<<endl;
-b++;
-}
+// void print(){
+// static int b =10;
+// cout<<b<<endl;
+// b++;
+// }
+int Bike::noOfBikes=0;
 int main() {
     Bike tvs(18);//object creation //constructor call
-    Bike honda(17.5);
-    Bike suzuki(19);
+   cout<<tvs.noOfBikes<<endl; //1
+   Bike honda(17.5);
+   cout<<honda.noOfBikes<<endl; //2
+   
+   Bike suzuki(19);
+   cout<<suzuki.noOfBikes<<endl;//3
     
     //static 
-    print();//10
-    print();//11
-    print();//12
+    // print();//10
+    // print();//11
+    // print();//12
 
 
     return 0;
