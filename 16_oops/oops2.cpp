e742c2e5c86d0b63b141ca2c1f2b86b6d763a4a0
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-07-11 08:35:32 +0530

    static variable

--- 16_oops/oops2.cpp
+++ 16_oops/oops2.cpp
@@ -16,16 +16,31 @@ Bike(int tyreSize,int engineSize=150){
 }
 
 
+
+
 //Destructor //calls when object goes out of scope
   
 ~Bike (){
     cout<<"destructor call hua";
 }
+
 };
+//static
+void print(){
+static int b =10;
+cout<<b<<endl;
+b++;
+}
 int main() {
     Bike tvs(18);//object creation //constructor call
     Bike honda(17.5);
     Bike suzuki(19);
-      
+    
+    //static 
+    print();//10
+    print();//11
+    print();//12
+
+
     return 0;
 }
