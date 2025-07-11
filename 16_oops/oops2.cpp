ca4590ce60a157f4d219bdbe46979a64900d4924
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-07-11 08:18:45 +0530

    Constructor

--- 16_oops/oops2.cpp
+++ 16_oops/oops2.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
+//constructors 
+class Bike{
+public:
+int tyreSize;
 
+//default constructor 
+// Bike(){}
+
+//parameterized constructor
+Bike(int tyreSize){
+    this->tyreSize=tyreSize;
+}
+};
 int main() {
-    
-
+    Bike tvs(18);//object creation //constructor call
+    Bike honda(17.5);
+    Bike suzuki(19);
       
     return 0;
 }
