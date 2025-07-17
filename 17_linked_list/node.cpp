17119b889db7efe4731f871bf8435acfd128378b
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-07-17 09:17:20 +0530

    Linked list

---
+++ 17_linked_list/node.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Node{
+
+    public:
+int val;
+Node* next;
+Node (int val){
+    this->val=val;
+    this->next=NULL;
+}
+
+};
+int main() {
+    Node a(1);
+    Node b(2);
+    a.next=&b;
+    Node c(3);
+    b.next=&c;
+cout<<a.next->val; //2
+cout<<b.next->val; //3
+    return 0;
+}
