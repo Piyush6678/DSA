0fdeef4cb9970ee7aa04dd25009b305991f4a1cc
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-08-27 05:38:22 +0530

    vector implementation

--- 18_stack/linkedlistimplementation.cpp
+++ 18_stack/linkedlistimplementation.cpp
@@ -1,5 +1,34 @@
 #include<iostream>
+using namespace std;
 class Node{
+    public:
 int val;
-Node* val;
+Node* next;
+Node(int val ){
+    this->val=val;this->next=NULL;
+}
+};
+class Stack{
+    Node* head;
+    int size;
+    Stack(){
+        head=NULL;size=0;
+    }
+void push(int val){
+    Node * temp=new Node(val);
+    temp->next=head;
+    head=temp;
+
+}
+void pop(){
+head=head->next;
+}
+void display(){
+     Node * temp=head;
+     while(temp){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+     }
+}
+
 };
