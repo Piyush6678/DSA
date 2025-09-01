4c6ee7161e58345cf6d24c058f796a97d5deaf75
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-09-01 14:30:38 +0530

    display

---
+++ queu/basics.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+//first in first out
+void display(queue<int>& q){
+    int n =q.size();
+    for(int i =1;i<=n;i++){
+        int x =q.front();
+        cout<<x<<" ";
+        q.pop();q.push(x);
+    }cout<<endl;
+} 
+void display_rev(queue<int>& q){
+    int n =q.size();
+    stack<int> st;
+    for(int i =1;i<=n;i++){
+       int x= q.front();
+       st.push(x);
+       q.pop();
+       q.push(x); 
+       
+       
+    }
+    for(int i =1;i<=n;i++){
+     cout<<  st.top()<<" ";
+       st.pop();
+       
+    }cout<<endl;
+} 
+int main() {
+    queue<int> q;
+    //push pop front size
+        q.push(10);
+        q.push(20);
+        q.push(30);
+        q.push(40);
+        q.push(50);
+        cout<<q.front();//10
+        cout<<q.back();//50
+        q.pop();
+        cout<<q.front();//20
+    return 0;
+}
