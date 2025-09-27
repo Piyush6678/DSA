25129d94bd841856f340825563a15c72d224a5dc
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-09-27 10:41:51 +0530

    ordered maps

--- 24_maps/ordered.cpp
+++ 24_maps/ordered.cpp
@@ -7,10 +7,20 @@ int main() {
     set<int> s;
     s.insert(1);
     s.insert(2);
-    s.insert(3); // sorted order
+    s.insert(3); // take logn time sorted order
     for(auto x:s){
         cout<<x<<" ";
-        
+
     }
+    //ordered map
+    map<int,int> m;
+   m[10]=1;
+   m[30]=2;
+   m[20]=3; // sorted by keys 
+    for(auto x:m){
+        cout<<x.first<<" ";
+
+    }
+
     return 0;
 }
