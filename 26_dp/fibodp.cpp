e4c2d567b90a37e3684837b69607f3312a343799
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-10-26 10:08:03 +0530

    bottom up dp

--- 26_dp/fibodp.cpp
+++ 26_dp/fibodp.cpp
@@ -10,8 +10,19 @@ if(dp[n]!=-1) {
 }
 return dp[n]=fib(n-1,dp)+fib(n-2,dp);
 }
+// iterative dp bootp dp tabulation
+void fibo(int n ){
+int arr[n+1]={0};
+arr[0]=0;
+arr[1]=1;
+for(int i =2;i<=n;i++){
+    arr[i]=arr[i-1]+arr[i+2];
+}
+}
 int main() {
     vector<int>dp(100,-1); // array should be greater the n+1 size
     cout<<fib(6,dp);
+
+
     return 0;
 }
