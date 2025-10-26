a4c91938bd1df7276e5103c5009c231d14af90f2
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-10-26 09:46:54 +0530

    memorizationin dp

--- 26_dp/fibodp.cpp
+++ 26_dp/fibodp.cpp
@@ -5,15 +5,10 @@ using namespace std;
 int fib(int n,vector<int>&dp ){
     if(n==1||n==0) return n;
     int ans2,ans1;
-if(dp[n-1]!=-1) ans1=dp[n-1];
-else { ans1 =fib(n-1,dp);
-dp[n-1]=ans1;}
-
-if(dp[n-2]!=-1) ans2=dp[n-2];   
-else { ans2=fib(n-2,dp);
-    dp[n-2]=ans2;
+if(dp[n]!=-1) {
+    return dp[n];
 }
-return  ans1+ans2;
+return dp[n]=fib(n-1,dp)+fib(n-2,dp);
 }
 int main() {
     vector<int>dp(100,-1); // array should be greater the n+1 size
