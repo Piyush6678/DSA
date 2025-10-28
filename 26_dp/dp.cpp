6c148ecb19c140f277aa9d12aeb87a57f8d40e45
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-10-28 10:53:15 +0530

    reccursive mincostclimb

--- 26_dp/dp.cpp
+++ 26_dp/dp.cpp
@@ -11,7 +11,7 @@ if(dp[n]!=-1) {
 return dp[n]=fib(n-1,dp)+fib(n-2,dp);
 }
 // iterative dp bootp dp tabulation
-void fibo(int n ){
+void fiboTabulation(int n ){
 int arr[n+1]={0};
 arr[0]=0;
 arr[1]=1;
@@ -19,6 +19,15 @@ for(int i =2;i<=n;i++){
     arr[i]=arr[i-1]+arr[i+2];
 }
 }
+int minCostClimbingStairshelper(vector<int> & cost,int i ,int sum){
+if(i>=cost.size()) return sum;
+return min(minCostClimbingStairshelper(cost,i+1,sum+cost[i]),minCostClimbingStairshelper(cost,i+2,sum+cost[i]));
+}
+int minCostClimbingStairs(vector<int> & cost,vector<int>?&dp){
+return min(minCostClimbingStairshelper(cost,0,0),minCostClimbingStairshelper(cost,1,0));
+}
+
+
 int main() {
     vector<int>dp(100,-1); // array should be greater the n+1 size
     cout<<fib(6,dp);
