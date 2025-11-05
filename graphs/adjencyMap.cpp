394bcc512c7ea122c9fb70d24318acd165764094
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-11-05 06:44:02 +0530

    adjecny map

---
+++ graphs/adjencyMap.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+# include<list>
+#include<vector> 
+#include<unordered_map> 
+using namespace std;
+
+vector <  unordered_map<int,int>>graph;
+int v;
+void add_edge(int src,int dest ,int wei, bool bi_dir=true)
+{
+    graph[src][dest]=wei;
+    if(bi_dir){
+        graph[dest][src]=wei;
+
+    }
+
+}
+
+void display()
+{
+    for (int i =0;i<graph.size();i++){
+        cout<<i<<"->";
+        for(auto ele:graph[i]){
+            cout<<"("<<ele.first<<" "<<ele.second<<" ) ,";
+
+
+        }
+        cout<<endl;
+    }
+}
+    int main(){
+cin>>v;
+graph.resize(v,unordered_map<int,int>());
+int e;
+cin>>e;
+while(e--){
+    int s,d,w;
+    cin>>s>>d>>w;
+    add_edge(s,d,w);
+
+}
+
+display();
+
+    return 0;
+}
