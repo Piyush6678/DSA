c180f4b30ea08c4fe4e5f57e5ebf04465f93a43b
Piyush6678 <aggarwalpiyush6678@gmail.com>
2025-11-11 19:48:15 +0530

    dsu union

--- 28_DSU/DSU.cpp
+++ 28_DSU/DSU.cpp
@@ -2,12 +2,22 @@
 #include <vector>
 
 using namespace std;
-void Union (){
-
-}
 int find(vector<int> &par,int x){
 // return the group/cluster x belongs to 
-return (par[x]==x)?x:find(par,par[x]);
+return par[x]=(par[x]==x)?x:find(par,par[x]);
+}
+void Union (vector<int>&par,vector<int>&rank,int a ,int b){
+int para=find(par,a);
+int parb=find(par,b);
+if(rank[para]>=rank[parb]){
+    rank[a]++;
+    par[b]=para;
+}else{
+    rank[b]++;
+    par[a]=parb;
+}
+
+
 }
 int main() {
     
